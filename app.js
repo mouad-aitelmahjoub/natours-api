11a6c08ad892d33f6b0e0df1d00bebde0425182e
mouad aem <mouad.aitelmahjoub@gmail.com>
2022-01-03 11:23:34 +0100

    initial Setup  : dotenv, mongoose, Db conection ✔

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const connectDB = require("./config/db")
+const dotenv = require("dotenv")
 const colors = require("colors")
+
+//Dotenv file
+dotenv.config()
+
+//Connect to Database
+connectDB()
 
 const app = express()
 
+//Use JSON middleware
+app.use(express.json())
+
 const PORT = 3000
 app.listen(PORT, () => {
-  console.log(`✔✔✔ App is running on port :  ${PORT} ✔✔✔ `.bgGreen)
+  console.log(`✔✔✔ App is running on port :  ${PORT} ✨🌟⭐`.bgGreen)
 })
