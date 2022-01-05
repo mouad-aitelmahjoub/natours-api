0e429b30c0a4692f7453317bb8db35eb9fcf9768
mouad aem <mouad.aitelmahjoub@gmail.com>
2022-01-05 11:32:53 +0100

    Global Error handling & AppError Class ✔

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const connectDB = require("./config/db")
 const dotenv = require("dotenv")
+const AppError = require("./utils/appError")
 const colors = require("colors")
 const tourRoutes = require("./routes/tourRoutes")
 
@@ -19,6 +20,27 @@ app.use(express.json())
 //Configure Routes
 app.use("/api/v1/tours", tourRoutes)
 
+//Unhandled routes
+app.all("*", (req, res, next) => {
+  const err = new AppError(`Can't find ${req.originalUrl} on this server!`, 404)
+
+  next(err)
+})
+
+//Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+
+  next()
+})
+
+//Launching server
 const PORT = 5000
 app.listen(PORT, () => {
   console.log(`✔✔✔ App is running on port :  ${PORT} ✨🌟⭐`.bgGreen)
