21a04a5867047e0e3b6110348bba81a504352433
mouad aem <mouad.aitelmahjoub@gmail.com>
2022-01-06 11:43:42 +0100

    Hashing Passwords ✔

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,27 @@ const userSchema = new mongoose.Schema({
   confirmPassword: {
     type: String,
     required: [true, "Please confirm your password"],
+    validate: {
+      //This Only works on CREATE & SAVE and NOT UPDATE!!!
+      validator: function (el) {
+        return el === this.password
+      },
+      message: "Your passwords dont match",
+    },
   },
+})
+
+userSchema.pre("save", async function (next) {
+  //Only run this function if password has been modified
+  if (!this.isModified("password")) return next()
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+
+  //Delete confirmPassword field in DB
+  this.confirmPassword = undefined
+
+  next()
 })
 
 const User = mongoose.model("User", userSchema)
